7937e606e0fb0126677d81c7d0d5b016b8cb4482
Patrick Goethe <goetheproductions@hotmail.com>
2018-02-09 08:25:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import App from './App';
+
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+
+const configureStore = (initialState) => {
+  const enhancer = compose(
+    applyMiddleware(
+      reduxThunk,
+      loggerMiddleware,
+    )
+  );
+  return createStore(reducers, initialState, enhancer);
+};
+
+const store = configureStore({});
+
+const AppWithRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('shipster', () => AppWithRedux);
