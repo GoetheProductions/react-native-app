5b8b708831b74c88fa3209ab709a46eacb81f466
Patrick Goethe <goetheproductions@hotmail.com>
2018-03-27 05:21:26 +0200

    os specific code

--- constants.js
+++ constants.js
@@ -1,1 +1,14 @@
+import techtable from './assets/images/techtable.jpeg';
+import gears from './assets/images/gears.jpeg';
+import foodtable from './assets/images/foodtable.jpeg';
+import coffeetable from './assets/images/coffeetable.jpeg';
+import bicycle from './assets/images/bicycle.jpg';
+
 export const API = 'http://localhost:3000/api/v1';
+export {
+  techtable,
+  gears,
+  foodtable,
+  coffeetable,
+  bicycle
+};
