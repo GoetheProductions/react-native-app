59c986f4d1451f1a26732737135088aafc40e68d
Patrick Goethe <goetheproductions@hotmail.com>
2018-04-05 14:50:45 +0200

    rearrange images

--- constants.js
+++ constants.js
@@ -1,1 +1,12 @@
+import techtable from './assets/images/techtable.jpeg';
+import gears from './assets/images/gears.jpeg';
+import foodtable from './assets/images/foodtable.jpeg';
+import coffeetable from './assets/images/coffeetable.jpeg';
+
 export const API = 'http://localhost:3000/api/v1';
+export {
+  techtable,
+  gears,
+  foodtable,
+  coffeetable
+};
